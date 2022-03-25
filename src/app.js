bd7649f0e6a3f479a7ee05674e5324416b39432b
Anderson <ands1776@gmail.com>
2022-03-25 10:38:01 -0300

    adicionando arquivo app

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import pedidosController from "./controllers/pedidos-controller.js";
+import generalMiddleware from "./middleware/general-middleware.js";
+import database from "./database/sqlite-db.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+generalMiddleware(app);
+
+pedidosController(app, database);
+
